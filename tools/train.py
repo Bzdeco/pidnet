4a860d71e226ddb79a1f48b73b93e67e4d971444
Jakub Gwizdała <jakub.gwizdala@gmail.com>
2024-05-22 09:57:48 +0200

    Run resumption

--- tools/train.py
+++ tools/train.py
@@ -2,7 +2,6 @@
 # Modified based on https://github.com/HRNet/HRNet-Semantic-Segmentation
 # ------------------------------------------------------------------------------
 import argparse
-import os
 
 from pathlib import Path
 from typing import Optional
@@ -16,12 +15,14 @@ from neptune.utils import stringify_unsupported
 from omegaconf import DictConfig
 from torch import nn
 from torch.cuda import amp
+from torch.cuda.amp import GradScaler
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 
 import models
 from configs import config
 from configs import update_config
+from powerlines.data import seed
 from tools import factory
 from utils.criterion import CrossEntropy, OhemCrossEntropy, BoundaryLoss
 from utils.function import train, validate
@@ -44,16 +45,21 @@ def default_commandline_arguments() -> DictConfig:
     })
 
 
-def run_training(config_powerlines: DictConfig) -> Optional[float]:  # returns optimized metric
-    args = default_commandline_arguments()
-    update_config(config, args)
+# returns optimized metric
+def run_training(
+    config_powerlines: DictConfig,
+    resume_run_id: Optional[int] = None,
+    resume_epoch: Optional[int] = None
+) -> Optional[float]:
+    update_config(config, default_commandline_arguments())
 
     import random
     print("Seeding with", SEED)
     random.seed(SEED)
     torch.manual_seed(SEED)
 
-    run = create_neptune_run(config_powerlines.name, resume=False, from_run_id=None)
+    resume = (resume_run_id is not None)
+    run = create_neptune_run(config_powerlines.name, resume=resume, from_run_id=resume_run_id)
     output_folder = checkpoint_folder(config_powerlines, run_id(run))
     run["config"] = stringify_unsupported(config_powerlines)
     run["config/pidnet"] = stringify_unsupported(dict(config))
@@ -71,7 +77,9 @@ def run_training(config_powerlines: DictConfig) -> Optional[float]:  # returns o
         num_workers=config_powerlines.data.num_workers.train,
         pin_memory=False,
         persistent_workers=True,
-        drop_last=True
+        drop_last=True,
+        # worker_init_fn=seed.seed_worker,
+        # generator=seed.torch_generator()
     )
 
     val_dataloader = DataLoader(
@@ -81,7 +89,9 @@ def run_training(config_powerlines: DictConfig) -> Optional[float]:  # returns o
         num_workers=config_powerlines.data.num_workers.val,
         pin_memory=False,
         persistent_workers=True,
-        drop_last=False
+        drop_last=False,
+        # worker_init_fn=seed.seed_worker,
+        # generator=seed.torch_generator()
     )
 
     # criterion
@@ -107,31 +117,23 @@ def run_training(config_powerlines: DictConfig) -> Optional[float]:  # returns o
     optimizer = factory.optimizer(config_powerlines, model)
     scaler = amp.GradScaler(enabled=True)
 
+    first_epoch = 0
+    if resume:
+        model, optimizer, scaler = load_checkpoint(resume_epoch, output_folder, model, optimizer, scaler)
+        first_epoch = resume_epoch + 1
+
     epoch_iters = int(len(train_dataset) / config_powerlines.data.batch_size.train)
-
-    last_epoch = 0
-    if config.TRAIN.RESUME:
-        model_state_file = os.path.join(output_folder, 'checkpoint.pth.tar')
-        if os.path.isfile(model_state_file):
-            checkpoint = torch.load(model_state_file, map_location={'cuda:0': 'cpu'})
-            last_epoch = checkpoint['epoch']
-            dct = checkpoint['state_dict']
-
-            model.module.model.load_state_dict({k.replace('model.', ''): v for k, v in dct.items() if k.startswith('model.')})
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            print("Loaded checkpoint (epoch {})".format(checkpoint['epoch']))
-
     n_epochs = config_powerlines.epochs
     num_iters = n_epochs * epoch_iters
     validation_config = config_powerlines.validation
 
     best_metric_value = -np.inf
-    for epoch in range(last_epoch, n_epochs):
+    for epoch in range(first_epoch, n_epochs):
         train(run, config_powerlines, epoch, epoch_iters, num_iters, train_dataloader, optimizer, scaler, model)
         if validation_config.every:
             metric_value = validate(n_epochs - 1, config, config_powerlines, run, val_dataloader, model)
             best_metric_value = max(best_metric_value, metric_value)
-        save_checkpoint(epoch, output_folder, model, optimizer)
+        save_checkpoint(epoch, output_folder, model, optimizer, scaler)
 
     if validation_config.last:
         return validate(n_epochs - 1, config, config_powerlines, run, val_dataloader, model)
@@ -139,18 +141,36 @@ def run_training(config_powerlines: DictConfig) -> Optional[float]:  # returns o
         return best_metric_value
 
 
-def save_checkpoint(epoch: int, folder: Path, model: nn.Module, optimizer: Optimizer, filename: Optional[str] = None):
+def save_checkpoint(
+    epoch: int, folder: Path, model: nn.Module, optimizer: Optimizer, scaler: GradScaler, filename: Optional[str] = None
+):
     filename = filename or f"{epoch:03d}.pt"
     torch.save({
         "epoch": epoch + 1,
         "state_dict": model.model.state_dict(),
         "optimizer": optimizer.state_dict(),
+        "scaler": scaler.state_dict()
     }, folder / filename)
+
+
+def load_checkpoint(epoch: int, folder: Path, model: nn.Module, optimizer: Optimizer, scaler: GradScaler):
+    filepath = folder / f"{epoch:03d}.pt"
+    print(f"Loading checkpoint from {filepath}")
+
+    checkpoint = torch.load(filepath)
+    model.model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+    if "scaler" in checkpoint:
+        scaler.load_state_dict(checkpoint["scaler"])
+
+    return model, optimizer, scaler
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--fold", required=False, type=int)
+    parser.add_argument("--resume", required=False, type=int)
+    parser.add_argument("--epoch", required=False, type=int)
     args = parser.parse_args()
 
     # Set k-fold CV fold if given
@@ -161,4 +181,4 @@ if __name__ == '__main__':
         powerlines_cfg.name = f"{powerlines_cfg.name}-fold-{fold}"
         powerlines_cfg.data.cv.fold = fold
 
-    run_training(powerlines_cfg)
+    run_training(powerlines_cfg, args.resume, args.epoch)
